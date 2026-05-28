814e373b9ada49e280993ef9b00a51bce34a756e
jovsnewbie6 <jovaniegador69@gmail.com>
2026-05-28 13:06:04 +0800

    Fixed app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
-from app import create_app
 
-app = create_app()
+database_url = os.getenv("DATABASE_URL")
 
-def _is_debug_mode():
-    return os.getenv("FLASK_DEBUG", "0") in {"1", "true", "True"}
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=_is_debug_mode())
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
