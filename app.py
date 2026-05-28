34b48e9d0c2b7bae4a715589154f64aa425b6373
jovsnewbie6 <jovaniegador69@gmail.com>
2026-05-28 15:17:23 +0800

    Updated app.py to fix the render

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db = SQLAlchemy(app)
+db.init_app(app)
+
+# IMPORT MODELS HERE
+from app.models import User, Student, StudentRecord, Permission
+
+with app.app_context():
+    db.create_all()
